11a59949759345c4c1e19e0bc215708cf4419df0
Adil Akhmetov <aa13081@auca.kg>
2026-01-28 23:12:27 +0600

    updated design added functions

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -30,14 +30,21 @@
         {
             this.components = new System.ComponentModel.Container();
             this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.epsilon = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.grid = new System.Windows.Forms.ComboBox();
+            this.number_of_problem = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.number_of_nodes = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // zedGraphControl1
             // 
-            this.zedGraphControl1.Location = new System.Drawing.Point(271, 26);
+            this.zedGraphControl1.Location = new System.Drawing.Point(206, 26);
             this.zedGraphControl1.Name = "zedGraphControl1";
             this.zedGraphControl1.ScrollGrace = 0D;
             this.zedGraphControl1.ScrollMaxX = 0D;
@@ -46,28 +53,19 @@
             this.zedGraphControl1.ScrollMinX = 0D;
             this.zedGraphControl1.ScrollMinY = 0D;
             this.zedGraphControl1.ScrollMinY2 = 0D;
-            this.zedGraphControl1.Size = new System.Drawing.Size(472, 377);
+            this.zedGraphControl1.Size = new System.Drawing.Size(582, 393);
             this.zedGraphControl1.TabIndex = 0;
             // 
-            // label1
+            // epsilon
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(35, 283);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(25, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "ε = ";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(67, 283);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 2;
+            this.epsilon.Location = new System.Drawing.Point(12, 299);
+            this.epsilon.Name = "epsilon";
+            this.epsilon.Size = new System.Drawing.Size(160, 20);
+            this.epsilon.TabIndex = 2;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(38, 348);
+            this.button1.Location = new System.Drawing.Point(12, 364);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(160, 55);
             this.button1.TabIndex = 3;
@@ -75,14 +73,97 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // grid
+            // 
+            this.grid.FormattingEnabled = true;
+            this.grid.Items.AddRange(new object[] {
+            "Равномерная сетка",
+            "Чебышёвская сетка"});
+            this.grid.Location = new System.Drawing.Point(12, 219);
+            this.grid.Name = "grid";
+            this.grid.Size = new System.Drawing.Size(160, 21);
+            this.grid.TabIndex = 4;
+            // 
+            // number_of_problem
+            // 
+            this.number_of_problem.FormattingEnabled = true;
+            this.number_of_problem.Items.AddRange(new object[] {
+            "1",
+            "6",
+            "9",
+            "11",
+            "14"});
+            this.number_of_problem.Location = new System.Drawing.Point(12, 42);
+            this.number_of_problem.Name = "number_of_problem";
+            this.number_of_problem.Size = new System.Drawing.Size(160, 21);
+            this.number_of_problem.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Задача";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 203);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Сетка";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 283);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Параметр (0, 1]";
+            // 
+            // number_of_nodes
+            // 
+            this.number_of_nodes.Location = new System.Drawing.Point(12, 132);
+            this.number_of_nodes.Name = "number_of_nodes";
+            this.number_of_nodes.Size = new System.Drawing.Size(160, 20);
+            this.number_of_nodes.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Количество узлов";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 116);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 13;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.number_of_nodes);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.number_of_problem);
+            this.Controls.Add(this.grid);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.epsilon);
             this.Controls.Add(this.zedGraphControl1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -94,9 +175,16 @@
         #endregion
 
         private ZedGraph.ZedGraphControl zedGraphControl1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox epsilon;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox grid;
+        private System.Windows.Forms.ComboBox number_of_problem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox number_of_nodes;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -11,22 +12,36 @@ using ZedGraph;
 
 namespace WindowsFormsApp1
 {
-
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
         }
-
-        private double f(double x, double eps)
+        private double get_value_of_function(double x, double eps, int ind_of_problem)
         {
-            return (1.0 - Math.Exp(-x / eps)) / (1.0 - Math.Exp(-1 / eps));
+            switch (ind_of_problem)
+            {
+                case 0:
+                    return (1.0 - Math.Exp(-x / eps)) / (1.0 - Math.Exp(-1 / eps));
+                case 1:
+                    return (x * Math.Sin(Math.PI * x / eps));
+                case 2:
+                    if (x == 0) return 0;
+                    else return x * Math.Log(1 / x);
+                case 3:
+                    if (0 <= x && x < 0.5 - eps) return 0.5;
+                    else if (0.5 - eps <= x && x < 0.5 + eps) return (1.0 - 2.0 * x) / (4.0 * eps);
+                    else return -0.5;
+                case 4:
+                    return x * x - eps;
+                default:
+                    return 0;
+            }
         }
-        private void button1_Click(object sender, EventArgs e)
-        {
-            double eps = Convert.ToDouble(textBox1.Text);
 
+        private void draw_function(int ind_of_problem, double eps)
+        {
             GraphPane pane = zedGraphControl1.GraphPane;
 
             pane.CurveList.Clear();
@@ -38,18 +53,23 @@ namespace WindowsFormsApp1
 
             for (double x = xmin; x <= xmax; x += 0.001)
             {
-                list.Add(x, f(x, eps));
+                list.Add(x, get_value_of_function(x, eps, ind_of_problem));
             }
 
             LineItem myCurve = pane.AddCurve("Sinc", list, Color.Green, SymbolType.None);
             myCurve.Line.Width = 3.0F;
 
             zedGraphControl1.AxisChange();
-
             zedGraphControl1.Invalidate();
-
-
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double eps = Convert.ToDouble(epsilon.Text);
+            int ind_of_problem = number_of_problem.SelectedIndex;
+            draw_function(ind_of_problem, eps);
+        }
+
     }
 }
 
