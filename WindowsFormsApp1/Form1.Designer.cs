c37ed56c76c51fc4ff556c3a25e5a458a0bf8f59
Adil Akhmetov <aa13081@auca.kg>
2026-02-01 14:27:02 +0600

    добавил интерполяцтю, равномерную и Чебушевскую сетку и ZedGraph.dll

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
             this.epsilon = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.grid = new System.Windows.Forms.ComboBox();
@@ -40,21 +39,8 @@
             this.number_of_nodes = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
             this.SuspendLayout();
-            // 
-            // zedGraphControl1
-            // 
-            this.zedGraphControl1.Location = new System.Drawing.Point(206, 26);
-            this.zedGraphControl1.Name = "zedGraphControl1";
-            this.zedGraphControl1.ScrollGrace = 0D;
-            this.zedGraphControl1.ScrollMaxX = 0D;
-            this.zedGraphControl1.ScrollMaxY = 0D;
-            this.zedGraphControl1.ScrollMaxY2 = 0D;
-            this.zedGraphControl1.ScrollMinX = 0D;
-            this.zedGraphControl1.ScrollMinY = 0D;
-            this.zedGraphControl1.ScrollMinY2 = 0D;
-            this.zedGraphControl1.Size = new System.Drawing.Size(582, 393);
-            this.zedGraphControl1.TabIndex = 0;
             // 
             // epsilon
             // 
@@ -149,11 +135,26 @@
             this.label5.Size = new System.Drawing.Size(0, 13);
             this.label5.TabIndex = 13;
             // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(198, 26);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(580, 393);
+            this.zedGraphControl1.TabIndex = 14;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.zedGraphControl1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.number_of_nodes);
@@ -164,7 +165,6 @@
             this.Controls.Add(this.grid);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.epsilon);
-            this.Controls.Add(this.zedGraphControl1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -173,8 +173,6 @@
         }
 
         #endregion
-
-        private ZedGraph.ZedGraphControl zedGraphControl1;
         private System.Windows.Forms.TextBox epsilon;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ComboBox grid;
@@ -185,6 +183,7 @@
         private System.Windows.Forms.TextBox number_of_nodes;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label5;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,11 +16,13 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        int ind_of_problem, ind_of_grid, num_of_nodes;
+        double eps;
         public Form1()
         {
             InitializeComponent();
         }
-        private double get_value_of_function(double x, double eps, int ind_of_problem)
+        private double get_value_of_function(double x)
         {
             switch (ind_of_problem)
             {
@@ -39,13 +43,46 @@ namespace WindowsFormsApp1
                     return 0;
             }
         }
-
-        private void draw_function(int ind_of_problem, double eps)
+        private double get_value_of_restored_function(double x, PointPairList nodes)
         {
-            GraphPane pane = zedGraphControl1.GraphPane;
-
-            pane.CurveList.Clear();
-
+            double ans = 0;
+            for (int i = 0; i < num_of_nodes; i++)
+            {
+                double tmp = 1;
+                for (int j = 0; j < num_of_nodes; j++)
+                {
+                    if (i == j) continue;
+                    tmp *= (x - nodes[j].X) / (nodes[i].X - nodes[j].X);
+                }
+                ans += tmp*nodes[i].Y;
+            }
+            return ans;
+        }
+        private PointPairList init_nodes()
+        {
+            PointPairList nodes = new PointPairList();
+            if (ind_of_grid == 0)
+            {
+                double step = 1.0 / (num_of_nodes - 1);
+                for(int i  = 0; i < num_of_nodes; i++)
+                {
+                    double x = Convert.ToDouble(i) * step;
+                    nodes.Add(x , get_value_of_function(x));
+                }
+                return nodes;
+            }
+            else
+            {
+                for (int i = 0; i < num_of_nodes; i++)
+                {
+                    double x = (1.0-Math.Cos(Math.PI*(2.0*Convert.ToDouble(i)+1)/(2.0* num_of_nodes)))/2.0;
+                    nodes.Add(x, get_value_of_function(x));
+                }
+                return nodes;
+            }
+        }
+        private void draw_initial_function(GraphPane pane)
+        {
             PointPairList list = new PointPairList();
 
             double xmin = 0;
@@ -53,21 +90,50 @@ namespace WindowsFormsApp1
 
             for (double x = xmin; x <= xmax; x += 0.001)
             {
-                list.Add(x, get_value_of_function(x, eps, ind_of_problem));
+                list.Add(x, get_value_of_function(x));
             }
 
-            LineItem myCurve = pane.AddCurve("Sinc", list, Color.Green, SymbolType.None);
+            LineItem myCurve = pane.AddCurve("Initial function", list, Color.Gray, SymbolType.None);
             myCurve.Line.Width = 3.0F;
-
-            zedGraphControl1.AxisChange();
-            zedGraphControl1.Invalidate();
         }
 
+        private void draw_nodes(GraphPane pane, PointPairList nodes)
+        {
+            LineItem myCurve = pane.AddCurve("Node", nodes, Color.Red, SymbolType.Circle);
+            myCurve.Line.IsVisible = false;
+            myCurve.Symbol.Fill = new Fill(Color.Blue);
+            myCurve.Line.Width = 8.0F;
+        }
+        private void draw_restored_function(GraphPane pane, PointPairList nodes)
+        {
+            PointPairList list = new PointPairList();
+
+            double xmin = 0;
+            double xmax = 1;
+
+            for (double x = xmin; x <= xmax; x += 0.001)
+            {
+                list.Add(x, get_value_of_restored_function(x, nodes));
+            }
+
+            LineItem myCurve = pane.AddCurve("Restored function", list, Color.Green, SymbolType.None);
+            myCurve.Line.Width = 3.0F;
+        }
         private void button1_Click(object sender, EventArgs e)
         {
-            double eps = Convert.ToDouble(epsilon.Text);
-            int ind_of_problem = number_of_problem.SelectedIndex;
-            draw_function(ind_of_problem, eps);
+            eps = Convert.ToDouble(epsilon.Text);
+            ind_of_problem = number_of_problem.SelectedIndex;
+            ind_of_grid = grid.SelectedIndex;
+            num_of_nodes = Convert.ToInt32(number_of_nodes.Text);
+            PointPairList nodes = init_nodes();
+
+            GraphPane pane = zedGraphControl1.GraphPane;
+            pane.CurveList.Clear();
+            draw_nodes(pane, nodes);
+            draw_restored_function(pane, nodes);
+            draw_initial_function(pane);
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
         }
 
     }
